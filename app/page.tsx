e7c29093cdb7898d681f15881177087d430fe315
방혜진 <hyejin@banghyejin-ui-MacBookAir.local>
2024-10-02 20:10:25 +0900

    add: 메인 페이지 페이지 이동 및 메타데이터 내용 추가
    - 페이지 이동: Link 를 이용한 페이지 이동
    - 이때, 라우터를 따로 설정하지 않았음에도 Link 를 이용해서 페이지를 이동할 수 있는 이유는 -> 폴더/page.tsx 이렇게 폴더 밑에 page 파일을 만들어주면 폴더명으로 된 라우터 경로가 자동으로 생성되기 때문
    - 메타 데이터는 html 에 작성하지 않아도 됨

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,50 @@
-import Image from "next/image";
+import Login from "./components/Login";
 
-export default function Home() {
+/**
+ * 페이지 이동
+ *
+ * 라우터 설정을 하지 않았음에도 링크를 이용해서 페이지를 이동할 수 있는 이유:
+ * 폴더/page.tsx 이렇게 폴더 밑에 page를 하나 만들어주면 자동으로 라우터가 생성이 되기 때문이다.
+ * 예시) about/page.tsx -> about 경로의 페이지가 생성됨
+ */
+import Link from "next/link";
+
+/**
+ * Meta Data
+ *
+ * 기존에는 html 파일 내, head 태그 안에 메타데이터를 정의했었는데,
+ * Next 에서는 html 에 작성하지 않아도 된다.
+ */
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next Study | Main",
+  description: "Next 공부 기록용 메인 페이지 입니다.",
+};
+
+const MainPage = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <h2 className="text-2xl flex justify-center p-20 border">메인 페이지</h2>
+      <div className="flex justify-center gap-20 p-4">
+        <li>
+          {/* about 으로 이동 */}
+          <Link href="/about" className="hover:font-bold">
+            about
+          </Link>
+        </li>
+        <li>
+          {/* test 로 이동 */}
+          <Link href="/test" className="hover:font-bold">
+            test
+          </Link>
+        </li>
+      </div>
+      <div className="flex justify-center py-20">
+        <Login />
+      </div>
     </div>
   );
-}
+};
+
+export default MainPage;
