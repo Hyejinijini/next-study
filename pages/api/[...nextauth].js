2c1202dae8b6fc7a31e958ce5695fa1130839ccd
HyeJin <155633287+Hyejinijini@users.noreply.github.com>
2024-10-03 20:15:55 +0900

    update: [...nextauth].js 파일 경로 변경
    - api/[...nextauth].js -> api/auth/[...nextauth].js

--- pages/api/[...nextauth].js
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-
-import Naver from "next-auth/providers/naver";
-import Google from "next-auth/providers/google";
-import Github from "next-auth/providers/github";
-
-export const authOptions = {
-  providers: [Naver({}), Google({}), Github({})],
-};
