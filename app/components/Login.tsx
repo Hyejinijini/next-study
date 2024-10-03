95061261487320eb7bb0277058b6853ea693daa3
HyeJin <155633287+Hyejinijini@users.noreply.github.com>
2024-10-03 20:12:13 +0900

    update: Login 기능 컴포넌트 구현
    - 로그인 상태에 따라 사용자에게 로그아웃 버튼 또는 로그인 버튼을 표시하는 Login 컴포넌트를 추가
    - 사용자가 로그인하면 환영 메시지와 함께 사용자 이름 및 이메일을 표시
    - NextAuth의 signIn 및 signOut 함수를 사용하여 인증 상태를 관리

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,8 +1,22 @@
-const Login = () => {
+"use client"; // 클라이언트 컴포넌트로 설정
+
+import { signIn, signOut } from "next-auth/react";
+
+const Login = ({ userInfo }: { userInfo: any }) => {
   return (
-    <div>
-      <button>로그인</button>
-    </div>
+    <>
+      {userInfo ? ( // userInfo 가 있을 경우 (로그인 상태)
+        <div>
+          <button onClick={() => signOut()}>로그아웃</button>
+          <div>
+            <h1>Welcome, {userInfo.name} 😀</h1>
+            <h3>Your email is {userInfo.email} !!</h3>
+          </div>
+        </div>
+      ) : (
+        <button onClick={() => signIn()}>로그인</button>
+      )}
+    </>
   );
 };
 
